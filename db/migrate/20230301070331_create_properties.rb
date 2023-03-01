c89e964ab80e2cd2d78056833ec5bba23b40c4f8
Brian <wamwangimathenge@gmail.com>
2023-03-01 11:23:04 +0300

    Properties migration modified

--- db/migrate/20230301070331_create_properties.rb
+++ db/migrate/20230301070331_create_properties.rb
@@ -3,7 +3,6 @@ class CreateProperties < ActiveRecord::Migration[6.1]
     create_table :properties do |t|
       t.string :image_url
       t.string :name
-      t.integer :rating
       t.integer :price
       t.string :address
       t.text :description
