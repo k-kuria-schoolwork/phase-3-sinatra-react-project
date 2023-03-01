2c43e26478606abbaedc94b7756588f3f554a546
Brian <wamwangimathenge@gmail.com>
2023-03-01 11:19:06 +0300

    Properties seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,18 @@ puts "🌱 Seeding spices..."
 # Seed your database here
 
 puts "Creating properties..."
-property1 = Property.create(name: "Riverside Green Villas", price: 57650000)
-property1 = Property.create(name: "Garden City Apartments", price: 325000000)
-property1 = Property.create(name: "Karen Blixen Cottages", price: 76500000)
-property1 = Property.create(name: "Kilimani Executive Apartments", price: 57800000)
-property1 = Property.create(name: "Muthaiga Heights", price: 43500000)
-property1 = Property.create(name: "Lavington Green Apartments", price: 69100000)
-property1 = Property.create(name: "Parklands Heights", price: 51250000)
-property1 = Property.create(name: "Kitisuru Terraces", price: 63550000)
-property1 = Property.create(name: "Langata Heights Villas", price: 38200000)
-property1 = Property.create(name: "Spring Valley Apartments", price: 45000000)
+property1 = Property.create(image_url: "https://unsplash.com/photos/RKdLlTyjm5g", name: "Riverside Green Villas", price: 57650000, address: "123 Riverside, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/2d4lAQAlbDA", name: "Garden City Apartments", price: 325000000, address: "456 Kasarani, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/yFV39g6AZ5o", name: "Karen Blixen Cottages", price: 76500000, address: "789 Karen, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/M7GddPqJowg", name: "Kilimani Executive Apartments", price: 57800000, address: "123 Kilimani, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/KtOid0FLjqU", name: "Muthaiga Heights", price: 43500000, address: "456 Muthaiga, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/5QLCohwVndQ", name: "Lavington Green Apartments", price: 69100000, address: "789 Lavington, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/UxCS2kVhZTc", name: "Parklands Heights", price: 51250000, address: "123, Parklands, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/7WsmtrrDZUc", name: "Kitisuru Terraces", price: 63550000, address: "456 Kitisuru, Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/iSmv0eEcHHM", name: "Langata Heights Villas", price: 38200000, address: "789 Langata Nairobi", description: "Lorem ipusr dolor...")
+property1 = Property.create(image_url: "https://unsplash.com/photos/2gDwlIim3Uw", name: "Spring Valley Apartments", price: 45000000, address: "123 Spring Valley, Nairobi", description: "Lorem ipusr dolor...")
+
+puts "Creating reviews"
+Review.create()
 
 puts "✅ Done seeding!"
