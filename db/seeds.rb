9ded910bcd84f57d2f6e99a8c549602dca15e920
Brian <wamwangimathenge@gmail.com>
2023-03-01 10:59:53 +0300

    Property seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,4 +2,16 @@ puts "🌱 Seeding spices..."
 
 # Seed your database here
 
+puts "Creating properties..."
+property1 = Property.create(name: "Riverside Green Villas", price: 57650000)
+property1 = Property.create(name: "Garden City Apartments", price: 325000000)
+property1 = Property.create(name: "Karen Blixen Cottages", price: 76500000)
+property1 = Property.create(name: "Kilimani Executive Apartments", price: 57800000)
+property1 = Property.create(name: "Muthaiga Heights", price: 43500000)
+property1 = Property.create(name: "Lavington Green Apartments", price: 69100000)
+property1 = Property.create(name: "Parklands Heights", price: 51250000)
+property1 = Property.create(name: "Kitisuru Terraces", price: 63550000)
+property1 = Property.create(name: "Langata Heights Villas", price: 38200000)
+property1 = Property.create(name: "Spring Valley Apartments", price: 45000000)
+
 puts "✅ Done seeding!"
