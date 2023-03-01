551863103763ce22b3177b1a743e9bff12c67a22
Brian <wamwangimathenge@gmail.com>
2023-03-01 12:11:49 +0300

    Review migration modified

--- db/migrate/20230301071054_create_reviews.rb
+++ db/migrate/20230301071054_create_reviews.rb
@@ -1,7 +1,7 @@
 class CreateReviews < ActiveRecord::Migration[6.1]
   def change
     create_table :reviews do |t|
-      t.integer :rating
+      t.integer :star_rating
       t.string :comment
       t.integer :property_id
     end
