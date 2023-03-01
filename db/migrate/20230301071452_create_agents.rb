c7e64cbbea19fc712db473f92f05dbe9b230df2e
Brian <wamwangimathenge@gmail.com>
2023-03-01 12:00:15 +0300

    Agents migration modified

--- db/migrate/20230301071452_create_agents.rb
+++ db/migrate/20230301071452_create_agents.rb
@@ -3,7 +3,6 @@ class CreateAgents < ActiveRecord::Migration[6.1]
     create_table :agents do |t|
       t.string :name
       t.integer :phone_number
-      t.integer :email
       t.integer :average_rating
       t.timestamps
     end
