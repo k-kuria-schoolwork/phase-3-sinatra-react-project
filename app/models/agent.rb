3dfc3071d12f08b55395ef7af4a9cf5777295350
charlotte karimi <[charlotte.karimi@student.moringaschool.com]>
2023-03-02 14:17:10 +0300

    changes in agent.rb

--- app/models/agent.rb
+++ app/models/agent.rb
@@ -1,7 +1,10 @@
 lass Agent < ActiveRecord::Base
 
-    has_many :reviews
-    has_many :property, through: :reviews
+    has_many :property
+
+    validates :username, presence: :true, uniqueness: true, exclusion: {in: ["admin", "superadmin", "user"]}
+    validates :email, presence: :true, uniqueness: :true, format: {with: /\A(?<username>[^@\s]+)@((?<domain_name>[-a-z0-9]+)\.(?<domain>[a-z]{2,}))\z/i}
+    validates :name, presence: true
 
     def favorite_property
         favorite_propery_id = Review.find_by(star_rating: reviews.maximum(:star_rating)).property_id
